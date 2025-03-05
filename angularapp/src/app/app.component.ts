3cc142049e23ff5e71216813358f167fd07fc28a
Garimathaku <garimathakur397@gmail.com>
2025-03-06 00:07:49 +0530

    [Garima]Add.Display hello bridgelabz with user input

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  standalone: true,
+  imports: [FormsModule] 
 })
 export class AppComponent {
-    
-   
-
-
+  title = 'angularapp';   
+  logoPath: string = "C:\Users\cw\OneDrive\Desktop\Angular_App\angularapp\src\assets\BL_logo_square_jpg.jpg";   
+  userName: string = "";
 
   openBridgeLabz() {
     window.open("https://www.bridgelabz.com", "_blank");
